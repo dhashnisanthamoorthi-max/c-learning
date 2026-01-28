19dccfd143b1d823e80c09c7bd495ad5e5abc45c
Dhashni <dhashnisanthamoorthi@gmail.com>
2026-01-28 22:27:32 +0530

    Add program to add two numbers using function

---
+++ functions/add_two_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    int x, y, result;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &x, &y);
+
+    result = add(x, y);
+
+    printf("Sum = %d\n", result);
+    return 0;
+}
