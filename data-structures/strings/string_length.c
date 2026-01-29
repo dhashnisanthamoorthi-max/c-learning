196777adad96030876ffd9f58765d5560cc7e665
Dhashni <dhashnisanthamoorthi@gmail.com>
2026-01-29 22:53:30 +0530

    Add program to find length of a string

---
+++ data-structures/strings/string_length.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int len = 0;
+
+    printf("Enter string: ");
+    scanf("%s", str);  // stops at space
+
+    while (str[len] != '\0') len++;
+
+    printf("Length: %d\n", len);
+
+    return 0;
+}
