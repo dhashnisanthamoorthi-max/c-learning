3f6fd79a8f4ffa2559eae2982b8bdae4bc4d1ad1
Dhashni <dhashnisanthamoorthi@gmail.com>
2026-02-04 21:49:12 +0530

    Add program to find minimum element in array

---
+++ data-structures/arrays/find_min_of_array.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int arr[5];
+    int min;
+
+    printf("Enter 5 numbers:\n");
+
+    for (int i = 0; i < 5; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    min = arr[0];
+
+    for (int i = 1; i < 5; i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+    }
+
+    printf("Minimum element = %d\n", min);
+
+    return 0;
+}
